18556d1b608f76f4d15026004f43c1ff5954a339
Collin M <57117254+Lavertas55@users.noreply.github.com>
2020-01-25 14:33:57 -0600

    Started adding openCV image processing

--- Vision.py
+++ Vision.py
@@ -9,52 +9,29 @@
 import json
 import time
 import sys
+from threading import Thread
+
 
 from cscore import CameraServer, MjpegServer, VideoSource, UsbCamera
 from networktables import NetworkTablesInstance
 import ntcore
+import cv2
+import numpy as np
+import math
 
-#   JSON format:
-#   {
-#       "team": <team number>,
-#       "ntmode": <"client" or "server", "client" if unspecified>
-#       "cameras": [
-#           {
-#               "name": <camera name>
-#               "path": <path, e.g. "/dev/video0">
-#               "pixel format": <"MJPEG", "YUYV", etc>   // optional
-#               "width": <video mode width>              // optional
-#               "height": <video mode height>            // optional
-#               "fps": <video mode fps>                  // optional
-#               "brightness": <percentage brightness>    // optional
-#               "white balance": <"auto", "hold", value> // optional
-#               "exposure": <"auto", "hold", value>      // optional
-#               "properties": [                          // optional
-#                   {
-#                       "name": <property name>
-#                       "value": <property value>
-#                   }
-#               ],
-#               "stream": {                              // optional
-#                   "properties": [
-#                       {
-#                           "name": <stream property name>
-#                           "value": <stream property value>
-#                       }
-#                   ]
-#               }
-#           }
-#       ]
-#       "switched cameras": [
-#           {
-#               "name": <virtual camera name>
-#               "key": <network table key used for selection>
-#               // if NT value is a string, it's treated as a name
-#               // if NT value is a double, it's treated as an integer index
-#           }
-#       ]
-#   }
+################### OPENCV IMAGE PROCESSING ######################
 
+image_width = 256
+image_height = 144
+
+diagonalView = math.radians(68.5)
+
+horizontalAspect = 16
+verticalAspect = 9
+
+diagonal Aspect = math.hypot(horizontalAspect, verticalAspect)
+
+################### FRC VISION IMAGE CODE (WEB INTERFACE) #######################
 configFile = "/boot/frc.json"
 
 class CameraConfig: pass
